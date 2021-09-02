148d10a471fafa981f363219491cd7e7aa659762
matthew <uiwwsw@icloud.com>
2021-09-02 11:26:46 +0900

    불필요 코드 제거
    Signed-off-by: matthew <uiwwsw@icloud.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,19 @@
-import { appWithTranslation } from 'next-i18next'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { NextPage } from 'next'
-import { wrapper } from '../core/redux/store'
-
-const ServiceStaterApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+import { appWithTranslation } from "next-i18next";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { NextPage } from "next";
+import { wrapper } from "../core/redux/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+const ServiceStaterApp: NextPage<AppProps> = ({
+  Component,
+  pageProps,
+}: AppProps) => {
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-          crossOrigin="anonymous"
-        />
-      </Head>
+      <Head>{}</Head>
       <Component {...pageProps} />
     </>
-  )
-}
+  );
+};
 
-export default wrapper.withRedux(appWithTranslation(ServiceStaterApp))
+export default wrapper.withRedux(appWithTranslation(ServiceStaterApp));
