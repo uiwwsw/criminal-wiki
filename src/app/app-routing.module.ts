ec97b1fe40e94d3c0ec20c772598e36790af5cc2
matthew <uiwwsw@icloud.com>
2021-10-11 15:04:58 +0900

    init
    Signed-off-by: matthew <uiwwsw@icloud.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainComponent } from './pages/main/main.component';
-import { DetailComponent } from './pages/detail/detail.component';
-const routes: Routes = [
-  {
-    path: '',
-    component: MainComponent,
-  },
-  {
-    path: 'detail',
-    component: DetailComponent,
-  },
-];
+
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-// import * as fromScoreboard from './reducers/scoreboard.reducer';
-// import * as fromProduct from './reducers/product.reducer';
-// import { ProductComponent } from './product/product.component';
-import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
-    FormsModule,
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    // StoreModule.forRoot({
-    //   game: fromScoreboard.reducer,
-    //   product: fromProduct.reducer,
-    // }),
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
